bb772f556479ec3095dc3605544847e7ce29d9c7
DanhDo-jcu <thanhdanh.do@my.jcu.edu.au>
2021-11-29 23:28:27 +0800

    Finalizing/reformatting codes & imperative comments.

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -2,13 +2,28 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:   print("Invalid score");
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    """Get score and return category."""
+    score = float(input("Enter score: "))
+    print(categorize_score(score))
+    random_score = random.uniform(1, 10)
+    print(f"Randomize score: {random_score:.2f}.\nResult: {categorize_score(random_score)}")
+
+
+def categorize_score(score):
+    """Categorize score and return result."""
+    if score < 0 or score > 100:
+        return "Invalid Score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+if __name__ == '__main__':
+    main()
--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,25 +1,34 @@
 """
 Add password check program
 """
+MIN_LENGTH = 10
+
+
+# Start of version1
+def version1():
+    password = input(f"Enter a valid password with at least {MIN_LENGTH} characters: ")
+    while len(password) < MIN_LENGTH:
+        print(f"Password does not meet the length requirement {len(password)}/{MIN_LENGTH} characters")
+        password = input("Re-enter new password: ")
+    print(len(password) * "*")
 
 
 def main():
-    """Start of main"""
-    password_length = 10
-    password = get_password(password_length)
+    """Get password and print asterisks."""
+    password = get_password(MIN_LENGTH)
     print_asterisks(password)
 
 
 def print_asterisks(password):
-    """Print asterisks in place of characters"""
+    """Print asterisks in place of all characters."""
     print(len(password) * "*")
 
 
-def get_password(password_length):
-    """Get and validate the password to fit the requirements"""
-    password = input("Enter a valid password: ")
-    while len(password) < password_length:
-        print(f"Password does not meet the length requirement {len(password)}/10 characters")
+def get_password(MIN_LENGTH):
+    """Get and validate the password to fit the requirements."""
+    password = input(f"Enter a valid password with at least {MIN_LENGTH} characters: ")
+    while len(password) < MIN_LENGTH:
+        print(f"Password does not meet the length requirement {len(password)}/{MIN_LENGTH} characters")
         password = input("Re-enter new password: ")
     return password
 
