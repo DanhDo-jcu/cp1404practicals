2779a06159dbbc6d9f39813ecd2be932e7fac057
DanhDo-jcu <thanhdanh.do@my.jcu.edu.au>
2021-11-29 22:41:23 +0800

    Refactor password check program to use some functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,9 +1,28 @@
 """
 Add password check program
 """
-password_length = 10
-password = input("Enter a valid password: ")
-while len(password) < password_length:
-    print(f"Password does not meet the length requirement {len(password)}/10 characters")
-    password = input("Re-enter new password: ")
-print(len(password) * "*")
+
+
+def main():
+    """Start of main"""
+    password_length = 10
+    password = get_password(password_length)
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    """Print asterisks in place of characters"""
+    print(len(password) * "*")
+
+
+def get_password(password_length):
+    """Get and validate the password to fit the requirements"""
+    password = input("Enter a valid password: ")
+    while len(password) < password_length:
+        print(f"Password does not meet the length requirement {len(password)}/10 characters")
+        password = input("Re-enter new password: ")
+    return password
+
+
+if __name__ == '__main__':
+    main()
